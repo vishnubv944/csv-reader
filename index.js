c4a8a271985d577eeb0f387d617929e524989505
Vishnu B V <vishnubv944@gmail.com>
2023-02-26 08:39:49 +0000

    ui design

---
+++ index.js
@@ -0,0 +1,10 @@
+function showTable(){
+    document.getElementById("file-input").classList.add("none")
+    document.getElementById("table").classList.remove("none")
+
+}
+
+function searchFunc(){
+    console.log("Searchingg...")
+
+}
